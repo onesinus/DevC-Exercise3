7928a496c483a518d3e32a87d92d9071a6756b4b
Onesinus SPT <onesinus231@gmail.com>
2019-08-27 21:02:19 +0700

    Thank you bang swapi, bang swapi selalu hepi wkwkwk filter udah jalan kalau press enter

--- main.js
+++ main.js
@@ -1,34 +1,43 @@
-function loadTableData(){
+function loadTableData(filter = ''){
 	fetch("https://swapi.co/api/planets")
 	  .then((resp) => resp.json())
-	  .then(function(data) {
-	  	console.log(data);
-	    addTr(data.results);
+	  .then(function(res) {
+	    addTr(filter,res.results);
       })
 	  .catch(function(error) {
-	  	console.log(error);
-	    // If there is any error you will catch them here
+	  	console.log("api ga tembus bro");
 	  });   
 }
 
-function addTr(data){
+function addTr(filter,data){	
+	console.log(data);
 	let tbody 	= document.getElementById("isiTable");	
+	tbody.innerHTML = ''; //clear Tbody first
+
 	for (let [key, value] of Object.entries(data)) {
 		let tr 		= document.createElement('tr');
+		let totalTdMatches = 0;
 		for (column in value) {
-			console.log(column);
+			if(value[column].indexOf(filter.toLowerCase()) !== -1){totalTdMatches++}
+
+			// console.log(column);
 			if(value.hasOwnProperty(column)){
 			    let td			= document.createElement('td');
-				if(column == "films" || column == "residents"){
+				if(column == "films" || column == "residents"){ // belum bisa atur widthnya, belum nampilin data
 					value[column] = value[column].join(";")
 					td.setAttribute("style", "width: 10px");
+				}else{				
+				    td.innerHTML 	= value[column];	
 				}
-			    td.innerHTML 	= value[column];	
 			    tr.appendChild(td);
 			}
 		}
 
-		tbody.appendChild(tr);
+		console.log(totalTdMatches);
+		// console.log(filter);
+		if(totalTdMatches > 0){ // show tr only if there are data matches
+			tbody.appendChild(tr);
+		}
 	}
 
 }
